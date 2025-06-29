e42440aa786ad035d9284930e73b707a7ab218e0
João V. <joao.vtr.oliveira@outlook.com>
2025-06-29 17:47:00 -0300

    feat: make the settings dialog show every timer config available

--- src/app/settings-dialog/settings-dialog.ts
+++ src/app/settings-dialog/settings-dialog.ts
@@ -1,15 +1,19 @@
 import {
 	ChangeDetectionStrategy,
 	Component,
+	effect,
 	inject,
+	signal,
 } from '@angular/core';
-import {
-	MatDialogModule,
-	MatDialogRef,
-} from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { ClockService } from '../services/clock.service';
+import {
+	ClockSettings,
+	TimerInterface,
+} from '../services/clockSettings/clock-settings';
 
 @Component({
 	standalone: true,
@@ -21,4 +25,16 @@ import { MatIconModule } from '@angular/material/icon';
 export class SettingsDialogComponent {
 	dialogRef = inject(MatDialogRef<SettingsDialogComponent>);
 
+	clockService = inject(ClockService);
+	clockSettings = inject(ClockSettings);
+
+	timerSelected = this.clockService.returnTimer();
+
+	checkTimers() {
+		return this.clockSettings.timers.filter(
+			(timer) =>
+				timer.timerStudy !== this.timerSelected.timerStudy &&
+				timer.timerRest !== this.timerSelected.timerRest
+		);
+	}
 }
