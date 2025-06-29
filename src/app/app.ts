9a7017aa78e7f4d7244201d88749dd3f79102189
João V. <joao.vtr.oliveira@outlook.com>
2025-06-29 17:07:13 -0300

    feat: implement the dialog in the app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,30 +1,51 @@
-import {ChangeDetectionStrategy, Component, computed, inject, OnInit, signal} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	computed,
+	inject,
+	OnInit,
+	signal,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { ClockService } from './services/clock.service';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatIconModule} from '@angular/material/icon';
-import { MusicPlayerComponent } from "./music-player/music-player";
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MusicPlayerComponent } from './music-player/music-player';
+import { MatDialog } from '@angular/material/dialog';
+import { SettingsDialogComponent } from './settings-dialog/settings-dialog';
 
 @Component({
-  selector: 'app-root',
-  imports: [MatCardModule, MatButtonModule, MatProgressSpinnerModule, MatIconModule, MusicPlayerComponent],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'app-root',
+	imports: [
+		MatCardModule,
+		MatButtonModule,
+		MatProgressSpinnerModule,
+		MatIconModule,
+		MusicPlayerComponent,
+	],
+	templateUrl: './app.html',
+	styleUrl: './app.css',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
-  clockService = inject(ClockService);
-  showMusic = signal(false);
+	clockService = inject(ClockService);
+	showMusic = signal(false);
+  
+  private dialog = inject(MatDialog);
 
-  onMusicClick() {
-    this.showMusic.update((oldMusic) => !oldMusic);
-  }
+	onMusicClick() {
+		this.showMusic.update((oldMusic) => !oldMusic);
+	}
 
-  formatTime(ms: number): string {
-    const totalSeconds = Math.floor(ms / 1000);
-    const min = String(Math.floor(totalSeconds / 60)).padStart(2, '0');
-    const sec = String(totalSeconds % 60).padStart(2, '0');
-    return `${min}:${sec}`;
-  }
+	openDialog() {
+		const dialogRef = this.dialog.open(SettingsDialogComponent);
+	}
+
+	formatTime(ms: number): string {
+		const totalSeconds = Math.floor(ms / 1000);
+		const min = String(Math.floor(totalSeconds / 60)).padStart(2, '0');
+		const sec = String(totalSeconds % 60).padStart(2, '0');
+		return `${min}:${sec}`;
+	}
 }
