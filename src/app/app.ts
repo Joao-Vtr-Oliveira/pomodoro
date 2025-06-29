0fb8a7d341a0ab109f70c62e8501b0e454fc39f1
João V. <joao.vtr.oliveira@outlook.com>
2025-06-29 12:27:21 -0300

    feat: implement the player in the app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,23 +4,18 @@ import {MatCardModule} from '@angular/material/card';
 import { ClockService } from './services/clock.service';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatIconModule} from '@angular/material/icon';
+import { MusicPlayerComponent } from "./music-player/music-player";
 
 @Component({
   selector: 'app-root',
-  imports: [MatCardModule, MatButtonModule, MatProgressSpinnerModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatProgressSpinnerModule, MatIconModule, MusicPlayerComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   clockService = inject(ClockService);
-
-  // controller = computed(() => {
-  //   if(this.clockService.timerController() === 'rest') return this.clockService.remainingTime() === this.clockService.timerRest();
-  //   return this.clockService.remainingTime() === this.clockService.timerLimit();
-    
-  // })
-
+  
   formatTime(ms: number): string {
     const totalSeconds = Math.floor(ms / 1000);
     const min = String(Math.floor(totalSeconds / 60)).padStart(2, '0');
